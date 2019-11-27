14a3700dde0d0c3f35a3b338764a4ebca2f11a11
ELREYS <gdilisa@hotmail.com>
2019-11-27 15:17:18 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
+//Mongo DB and Express API
+const mongoose = require('mongoose');
 const express = require('express');
 
 const app = express();
+
+const mongoUri = 'mongodb+srv://admin:admin@cluster0-zcwbt.mongodb.net/test?retryWrites=true&w=majority'
+mongoose.connect(mongoUri,{
+    useNewUrlParser:true,
+    useCreateIndex:true,
+})
+
+mongoose.connection.on('connected',() => {
+    console.log("Connected to mongo instances");
+    
+});
+
+mongoose.connection.on('error',(err) =>{
+    console.log("Error connecting to mongo",err);
+    
+})
+
 
 app.get('/',(req,res) =>{
     res.send("Hi There");
