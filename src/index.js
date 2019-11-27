0a21d48186208843bb20652f83afb64b04a1b8ba
ELREYS <gdilisa@hotmail.com>
2019-11-27 15:54:24 +0100

    add navigation router file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 //Mongo DB and Express API
 const mongoose = require('mongoose');
 const express = require('express');
-
+const authRouter = require('./routes/authRoutes');
 const app = express();
+
+app.use(authRouter);
 
 const mongoUri = 'mongodb+srv://admin:admin@cluster0-zcwbt.mongodb.net/test?retryWrites=true&w=majority'
 mongoose.connect(mongoUri, {
@@ -27,7 +29,7 @@ mongoose.connection.on('error',(err) =>{
 
 
 app.get('/',(req,res) =>{
-    res.send("Hi There");
+    res.send("Hi There im here");
 });
 
 app.listen(3000,() => {
